9f789c3c387702d5534f1b414b5dbd96c2fd98ca
Rhoda P <rhodaphillipsosei@gmail.com>
2022-06-14 12:41:09 -0400

    initial commit - set up server & client

---
+++ server/server.js
@@ -0,0 +1,12 @@
+PORT = 5000
+const express = require('express')
+const app = express()
+
+
+app.get("/api", (req, res) => {
+    res.json({"users": ["userOne", "userTwo", "userThree"]})
+})
+
+app.listen(5000, ()=> {
+    console.log(`Server is listening on port ${PORT}`)
+})
